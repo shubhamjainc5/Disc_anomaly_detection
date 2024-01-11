689ff4d70f12d1812e352b71fc0fe6ca08d2a1d2
shubhamjainc5 <shubham.jain@course5i.com>
2024-01-11 13:30:38 +0530

    baseline forecasting model with narrative

---
+++ openai_processor_earlywarning.py
@@ -0,0 +1,193 @@
+"""
+This script prepares request for GPT-3 APIs, Hits the API and returns the response.
+@author: Sujith R Kumar
+
+Returns:
+    _type_: _description_
+"""
+
+import json
+import numpy as np
+import os
+import re
+import copy
+from logging_handler import Logger
+from langchain.schema import OutputParserException
+# openai is commented to avoid langchain resource not found error
+#import openai
+
+from typing import List, Dict
+# from utils.timer import timed
+import traceback
+
+from json.decoder import JSONDecodeError
+from openai.error import APIError, Timeout, RateLimitError, APIConnectionError, AuthenticationError, \
+    ServiceUnavailableError, InvalidRequestError
+
+from langchain.llms import AzureOpenAI
+from langchain.chat_models import AzureChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+# from langchain.chains import SequentialChain, TransformChain
+# from langchain.memory import SimpleMemory
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.cache import SQLiteCache
+from langchain import globals
+
+from langchain.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field, validator
+from typing import List
+
+import ast
+
+from prompt import  forecasting_sys_prompt, forecasting_prompt
+
+
+with open("config.json", "r") as f:
+    domain_config = json.load(f)["openai_cred"]
+
+
+# Set llm_cache
+globals.set_llm_cache(SQLiteCache(database_path="ew_cache.db"))
+
+
+os.environ["OPENAI_API_TYPE"] = domain_config['OPENAI_API_TYPE']
+os.environ["OPENAI_API_VERSION"] = domain_config['OPENAI_API_VERSION']
+os.environ["OPENAI_API_BASE"] = domain_config['OPENAI_API_BASE']
+os.environ["OPENAI_API_KEY"] = domain_config['OPEN_AI_API_KEY']
+
+
+class Narrative(BaseModel):
+
+    anomaly_date: str = Field(description="anomaly dates which were present in table data")
+    narrative: str = Field(description="narrative with respect to selected anomaly date")
+    narrative_html: str = Field(description="narrative in html format")
+
+
+class ForecastNarrativePlainText(BaseModel):
+
+    ForecastNarrative: List[Narrative] = Field(description="a set of anomalous points with their date and it's narrative in dictionary format")
+
+
+def generate_ew_narrative(table_data:str,anomaly_dates:List, use_cache:bool) -> (List[Dict[str,str]], int, float):
+    print('\n---------Early warning-------------\n')
+
+    cnt = 0
+    call_tokens = []
+
+    def reason_llm_call() -> List[Dict[str,str]]:
+
+        nonlocal cnt
+
+        model = AzureChatOpenAI(
+            deployment_name=domain_config['AZURE_DEPLOYMENT_NAME'],
+            model_name=domain_config['AZURE_MODEL_NAME'],
+            max_tokens=2000,
+            temperature=0.5,
+            verbose=True,
+            request_timeout=20,
+            cache= use_cache
+        )
+
+        system_message_prompt = SystemMessagePromptTemplate.from_template(forecasting_sys_prompt)
+
+        reason_parser = PydanticOutputParser(pydantic_object=ForecastNarrativePlainText)
+        reason_format_instructions = reason_parser.get_format_instructions()
+
+        reason_template = PromptTemplate(input_variables=["table_data"], template=forecasting_prompt,
+                                        partial_variables={"format_instructions": reason_format_instructions})
+        
+        reason_prompt = reason_template.format(table_data=table_data, format_instructions = reason_format_instructions)
+        # print(reason_prompt)
+
+        human_message_prompt = HumanMessagePromptTemplate(prompt=reason_template)
+
+        chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+
+        reason_chain = LLMChain(llm=model, prompt=chat_prompt, output_key="generated_narrative")
+
+        gpt_op = reason_chain({"table_data": table_data})
+        Logger.info("\n Response from GPT : {0}".format(gpt_op))
+
+        generated_reasons = gpt_op['generated_narrative']
+        # print(generated_reasons)
+
+        reason_ip_tokens = model.get_num_tokens(reason_prompt)
+        reason_op_tokens = model.get_num_tokens(generated_reasons)
+        call_tokens.append(reason_ip_tokens + reason_op_tokens)
+        cnt += 1
+
+        try:
+            reason_result = reason_parser.parse(generated_reasons).json()
+            reason_result = json.loads(reason_result)['AnomalyReasoning']
+            Logger.info('\n Langchain json reason content parsed: {0}'.format(reason_result))
+        except:
+            reason_result = generated_reasons[generated_reasons.find('['): generated_reasons.rfind(']') + 1]
+            reason_result = ast.literal_eval(reason_result)
+            Logger.info('\n Regex json reason content parsed: {0}'.format(reason_result))
+
+        gpt_dt = [ res["anomaly_date"] for res in reason_result]
+
+        if set(gpt_dt) == set(anomaly_dates) and len(gpt_dt) == len(anomaly_dates):
+            pass
+        else:
+            raise AssertionError(f"Count of anamolous dates in LLM output does not match with provided dates.")
+
+        return reason_result
+
+    try:
+        result = reason_llm_call()
+        status = 'LLMParsed'
+    except (AssertionError, JSONDecodeError, OutputParserException, KeyError) as e:
+        Logger.error(traceback.format_exc())
+        Logger.error('ERROR: reason Parsing or Assertion Failed')
+        try:
+            globals.get_llm_cache().clear()
+            result = reason_llm_call()
+            status = 'LLMRetryParsed'
+        except (APIError, Timeout, ServiceUnavailableError) as e1:
+            Logger.error(traceback.format_exc())
+            Logger.error('ERROR: LLM SERVER Not responding')
+            result = []
+            status = 'LLMServerError'
+            globals.get_llm_cache().clear()
+        except (RateLimitError, APIConnectionError, InvalidRequestError, AuthenticationError) as e3:
+            Logger.error(traceback.format_exc())
+            Logger.error('ERROR: Application server Not responding')
+            result = []
+            status = 'AppServerError'
+            globals.get_llm_cache().clear()
+        except:
+            Logger.error(traceback.format_exc())
+            Logger.error('UNKNOWN ERROR: occurred')
+            result = []
+            status = 'UnknownError'
+            globals.get_llm_cache().clear()
+    except (APIError, Timeout, ServiceUnavailableError) as e1:
+        Logger.error(traceback.format_exc())
+        Logger.error('ERROR: LLM SERVER Not responding')
+        result = []
+        status = 'LLMServerError'
+        globals.get_llm_cache().clear()
+    except (RateLimitError, APIConnectionError, InvalidRequestError, AuthenticationError) as e3:
+        Logger.error(traceback.format_exc())
+        Logger.error('ERROR: Application server Not responding')
+        result = []
+        status = 'AppServerError'
+        globals.get_llm_cache().clear()
+    except:
+        Logger.error(traceback.format_exc())
+        Logger.error('UNKNOWN ERROR: occurred')
+        result = []
+        status = 'UnknownError'
+        globals.get_llm_cache().clear()
+
+    print('\n---------reason:{0}-------------\n'.format(status))
+    Logger.info(f"\n---------reason:{status}-------------\n")
+
+    return result, cnt, 0 if len(call_tokens) == 0 else np.average(call_tokens), status
--- prompt.py
+++ prompt.py
@@ -43,51 +43,38 @@ Please make sure you always abide by the following rules to create reasoning in 
 Output JSON:
 """
 
-# html_sys_prompt = """Given a casual insight data, your job is to analyse and create a correct detailed causal reasoning behind some of the anomalous instances in data indicated by anomaly flag in the data."""
+forecasting_sys_prompt = """Given a time series table data of product sales, your job is to create a narrative for the given data."""
 
-# html_beautify_prompt = """Given an insight, beautify it with HTML tags based on the provided rules.
+forecasting_prompt = """
+Below I am providing you table data in comma seperated format :
+{table_data}
 
-# Please make sure you always abide by the following rules:
-# - ALWAYS allowed to use two colors [red, green] only.
-# - ALWAYS use red color for highlighting decreasing % numeric values only.
-# - ALWAYS use green color for highlighting increasing % numeric values only.
-# - ALWAUS use bold tags for highlighting metrics, dimensions and dates values only.
-# - ALWAYS use ordered list tags for numeric pointers as (1., 2., 3.) whenever necessary.
+Above table contains data for a KPI, KPI's moving window average  and the percentage of difference between kpi's actual value and kpi's moving window average value.
 
-# Be sure you MUST NOT change the actual meaning of insight.
+here is just an example to show how the narrative for anomalous points should look like:
+'''
+"ForecastNarrative":[
+Dict(
+"anomaly_date":"2021-12-13",
+"narrative": "The shipped revenue is expected to (drop) by -14.04% below the average during the week starting (2021-12-13). Its predicted value for that week is 91959.359. Please take required remedial actions.",
+"narrative_html": "The <span style='font-weight:bold;'>Shipped Revenue</span> is expected to (<span style='font-weight:bold;'>drop</span>) by <span style='color:red;font-weight:bold;'>14.04</span>% below the average during the week starting (<span style='font-weight:bold;'>2021-12-13</span>). Its predicted value for that week is <span style='font-weight:bold;'>91959.359</span> . Please take required remedial actions."
+)
+            ]
+'''
 
-# Insight:
-# ```
-# The data shows the trend of Shipped Revenue in the United States from 2023-07-03 to 2023-08-21. The Shipped Revenue for the Last week (starting 2023-08-21) was $23.66 M, which is a decrease of 4.4% compared to the immediate previous week (starting 2023-08-14). Moreover,
+Please make sure you always abide by following rules:
+- You MUST predict detailed narrative for each anomalous points only based on the provided data only. 
+- You SHOULD try to predict narrative in maximum 100 to 300 words only.
+- ALWAYS think step by step for predicting correct narrative.
 
-# 1. The Shipped Revenue for the Last week shows an increase of 59.5% compared to the first week (starting 2023-07-03) revenue $14.83 M.
-# 2. The Shipped Revenue for the Last week shows an increase of 15.8% compared to the second week (starting 2023-07-10) revenue $20.43 M.
-# 3. The Shipped Revenue for the Last week shows an increase of 5.8% compared to the third week (starting 2023-07-17) revenue $22.36 M.
-# 4. The Shipped Revenue for the Last week shows a decrease of 6.1% compared to the fourth week (starting 2023-07-24) revenue $25.19 M.
-# 5. The Shipped Revenue for the Last week shows an increase of 14.9% compared to the fifth week (starting 2023-07-31) revenue $20.59 M.
-# 6. The Shipped Revenue for the Last week shows a decrease of 2.9% compared to the sixth week (starting 2023-08-07) revenue $24.37 M.
+Please make sure you always abide by the following rules to create reasoning in html format:
+- ALWAYS allowed to use two colors [red, green] only.
+- ALWAYS use red color for highlighting decreasing % numeric values only.
+- ALWAYS use green color for highlighting increasing % numeric values only.
+- ALWAUS use bold tags for highlighting metrics, dimensions and dates values only.
+- Be sure you MUST NOT change the actual meaning of narrative while converting it into html format.
 
-# Overall, the average Shipped Revenue for all weeks is $22.02 M and a standard deviation of $3.20 M. The trend of Shipped Revenue in the United States for the last week is 4.4% lower than the immediate previous week, indicating a downward trend.
-# ```
-# AI: <p>The data shows the trend of <b>Shipped Revenue</b> in the <b>United States</b> from <b>2023-07-03</b> to <b>2023-08-21</b>. The <b>Shipped Revenue</b> for the last week (starting <b>2023-08-21</b>) was <span style='color:red;'>$23.66 M</span>, which is a decrease of <span style='color:red;'>4.4%</span> compared to the immediate previous week (starting <b>2023-08-14</b>). Moreover,</p> <br> <ol> <li>The <b>Shipped Revenue</b> for the last week shows an increase of <span style='color:green;'>59.5%</span> compared to the first week (starting <b>2023-07-03</b>) revenue $14.83 M. </li> <br> <li>The <b>Shipped Revenue</b> for the last week shows an increase of <span style='color:green;'>15.8%</span> compared to the second week (starting <b>2023-07-10</b>) revenue $20.43 M. </li> <br> <li>The <b>Shipped Revenue</b> for the last week shows an increase of <span style='color:green;'>5.8%</span> compared to the third week (starting <b>2023-07-17</b>) revenue $22.36 M. </li> <br> <li>The <b>Shipped Revenue</b> for the last week shows a decrease of <span style='color:red;'>6.1%</span> compared to the fourth week (starting <b>2023-07-24</b>) revenue $25.19 M. </li> <br> <li>The <b>Shipped Revenue</b> for the last week shows a increase of <span style='color:green;'>14.9%</span> compared to the fifth week (starting <b>2023-07-31</b>) revenue $20.59 M. </li> <br> <li>The <b>Shipped Revenue</b> for the last week shows a decrease of <span style='color:red;'>2.9%</span> compared to the sixth week (starting <b>2023-08-07</b>) revenue $24.37 M. </li> </ol><br><p>Overall, the average <b>Shipped Revenue</b> for all weeks is <span style='color:green;'>$22.02 M</span> and a standard deviation of <span style='color:green;'>$3.20 M</span>. The trend of <b>Shipped Revenue</b> in the <b>United States</b> for the last week is <span style='color:red;'>4.4%</span> lower than the previous dates, indicating a downward trend.</p>
-
-# Insight:
-# ```
-# The data shows the trend of Shipped Revenue in the United States from 2023-07-03 to 2023-08-21. The Shipped Revenue for the last week (2023-08-21) was $23.65 M, a decrease of 4.4% compared to the immediate previous week (starting 2023-08-14). The average Shipped Revenue for all weeks is $22.84 M with a standard deviation of $7.84 M. The trend of Shipped Revenue for the last week is lower than the immediate previous week, indicating a negative trend.
-# ```
-# AI: <p>The data shows the trend of <b>Shipped Revenue</b> in the <b>United States</b> from <b>2023-07-03</b> to <b>2023-08-21</b>. The <b>Shipped Revenue</b> for the last week (<b>2023-08-21</b>) was <span style='color:red;'>$23.65 M</span>, a decrease of <span style='color:red;'>4.4%</span> compared to the immediate previous week (starting <b>2023-08-14</b>). The average <b>Shipped Revenue</b> for all weeks is <span style='color:green;'>$22.84 M</span> with a standard deviation of <span style='color:green;'>$7.84 M</span>. The trend of <b>Shipped Revenue</b> for the last week is lower than the immediate previous week, indicating a negative trend.</p>
-
-# Insight:
-# ```
-# The Shipped Revenue in the United States for the Last Year (2023-01-01) has increased compared to the previous year (2022-01-01 to 2022-12-31). This can be attributed to the increase in Booked Revenue ($727.64 M) for the Last year, decrease in Cancel Revenue ($36.47 M) for the Last year and increase in Visits (43.32 M) for the Last year. 
-# Overall, these factors have caused the Shipped Revenue to increase for the Last Year compared to the previous year.
-# ```
-# AI: <p>The <b>Shipped Revenue</b> in the <b>United States</b> for the Last Year (<b>2023-01-01</b>) has increased compared to the previous year (<b>2022-01-01</b> to <b>2022-12-31</b>). This can be attributed to the increase in <b>Booked Revenue</b> (<span style='color:green;'>$727.64 M</span>) for the Last year, decrease in <b>Cancel Revenue</b> (<span style='color:red;'>$36.47 M</span>) for the Last year and increase in <b>Visits</b> (<span style='color:green;'>43.32 M</span>) for the Last year.</p><br><p>Overall, these factors have caused the <b>Shipped Revenue</b> to increase for the Last Year compared to the previous year.</p>
-
-# Insight:
-# ```
-# {insight}
-# ```
-# AI:
-# """
+\n{format_instructions}
+Output JSON:
+"""
 
